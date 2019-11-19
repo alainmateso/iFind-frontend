0a5bc93f37395eb2ba7e6fb9d4fd6c8833d9bde3
Alain <alainmateso@gmail.com>
2019-11-19 17:30:48 +0200

    Chore(Sass): add sass preprocessor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
+import './App.scss';
 import { Provider } from 'react-redux';
 import Store from './reduxStore';
 import HomeComponent from './components/homeComponent';
-=======
-import logo from './zoom.png';
-import './App.scss';
->>>>>>> Chore(Sass): add sass preprocessor
 
 function App() {
   return (
