a83c4e05875912de43c435eb93e09b07d3191644
sabin18 <sabin6341@gmail.com>
2019-11-19 12:53:01 +0200

    ch(add story book):add story book - configure storybook [Finishes#169834743]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { Provider } from 'react-redux';
 import Store from './reduxStore';
 import HomeComponent from './components/homeComponent';
+import logo from './zoom.png';
 
 function App() {
   return (
