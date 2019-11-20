d90f012a9a3e8a2f9044525b092dda5db22041f4
nignanthomas <nignanthomas@gmail.com>
2019-11-20 10:01:43 +0200

    chore(travis): Setting Travis
    -create .travis.yml to run the test on travis and pass to coveralls
    
    [Finishes #169834721]

--- src/components/homeComponent.js
+++ src/components/homeComponent.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { defaultAction } from '../actions/defaultActions';
-import logo from './../zoom.png';
+import PropTypes from 'prop-types';
+import defaultAction from '../actions/defaultActions';
+import logo from '../zoom.png';
 
 
 class HomeComponent extends Component {
-    componentDidMount() {
-        this.props.defaultAction();
-    }
-    render() {
-        return (
-            <div className="App" >
+  componentDidMount() {
+    this.props.defaultAction();
+  }
+
+  render() {
+    const { text } = this.props;
+    return (
+            <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
                         Welcome to iFind!
                     </p>
-                    <h1>{this.props.text}</h1>
+                    <h1>{text}</h1>
                 </header>
             </div>
-        );
-    }
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    text: state.default.text
+HomeComponent.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+
+const mapStateToProps = (state) => ({
+  text: state.default.text,
 });
 export default connect(mapStateToProps, { defaultAction })(HomeComponent);
