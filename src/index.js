6eb1047fd1230729b214422eade91d24ca0943f5
nignanthomas <nignanthomas@gmail.com>
2019-11-21 16:30:45 +0200

    feature(signup): Register Feature
    - create SignupComponent
    
    - create Auth.scss to style the UsersCompnent
    
    - crate routes to load SignupComponent and LoginComponent
    
    [Finishes #169861484]

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import Users from './components/Users';
+import Users from './components/UsersComponent';
+import NotFound from './components/NotFound';
 
 const routing = (
     <Router>
         <div>
             <Route exact path="/" component={App} />
             <Route path="/users" component={Users} />
+            <Route path="*" omponent={NotFound} />
         </div>
     </Router>
 );
